ae2d493944b67d3d081bf4e7416fb89d6ef52ebf
rodrigoC <rodrigo.antonio.caro@gmail.com>
2024-07-10 11:24:39 -0400

    proyecto creado con spring initializr

---
+++ src/main/java/api/foro_hub_rodrigo/ForoHubRodrigoApplication.java
@@ -0,0 +1,13 @@
+package api.foro_hub_rodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubRodrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubRodrigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/foro_hub_rodrigo/ForoHubRodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package api.foro_hub_rodrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubRodrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
